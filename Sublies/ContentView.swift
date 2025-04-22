e879027ee5e7fc6d0b9facf6432f9f17a5854913
Oliverio D <oliveriodbs@gmail.com>
2025-04-21 22:52:00 -0600

    list added

--- Sublies/ContentView.swift
+++ Sublies/ContentView.swift
@@ -7,15 +7,29 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+struct Subscription: Identifiable{
+    let id = UUID()
+    let name: String
+    let price: String
+    let dueDate: String
+}
+
+let sampleSubs=[
+    Subscription(name: "Netflix", price: "15.99", dueDate: "2025-05-22"),
+    
+    Subscription(name: "Spotify", price: "5.99", dueDate: "2025-05-22")
+]
+
+struct ContentView: View{
+    var body: some View{
+        Text("Subs")
+            .font(.system(size: 34, weight: .bold))
+        List(sampleSubs){ sub in
+            VStack(alignment:.leading){
+                Text(sub.name).font(.headline)
+                Text("\(sub.price) due on \(sub.dueDate)").font(.subheadline)
+            }
         }
-        .padding()
     }
 }
 
